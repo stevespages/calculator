b96a4928cbba84598e336def5c26baa02e6af183
Steve Greig <greigSteve@gmail.com>
2017-11-22 17:30:54 +0000

    showCalculatorForm function introduced. +, -, *, / all working. C (reset) working.

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-
-If ($_SERVER['REQUEST_METHOD'] != 'POST')	{
-$operand_1 = "";
-$operator = "";
-$operand_2 = "";
-$answer = "";
-
-$calulator_form = "
+function showCalculatorForm($operand_1, $operator, $operand_2, $answer) {
+	$calulator_form = "
 <form method='post'>
 <input type='text' name='operand_1' value=$operand_1>
 <input type='text' name='operator' value=$operator>
@@ -28,7 +22,19 @@ $calulator_form = "
 <input type='submit' name='multiply' value='*'></br>
 <input type='submit' name='divide' value='/'>
 <input type='submit' name='equals' value='='>
+<p><a href=''>C</a></p>
 </form>";
+	echo $calulator_form;
+}
+
+If ($_SERVER['REQUEST_METHOD'] != 'POST')	{
+$operand_1 = "";
+$operator = "";
+$operand_2 = "";
+$answer = "";
+
+showCalculatorForm($operand_1, $operator, $operand_2, $answer);
+
 } else {
 	
 $operand_1 = $_POST['operand_1'];
@@ -42,6 +48,21 @@ $operand_1 = $_POST['n'];
 }
 }
 
+if($_POST['operand_1']!="") {
+if (isset($_POST['plus'])) {
+$operator = $_POST['plus'];
+}
+if (isset($_POST['minus'])) {
+$operator = $_POST['minus'];
+}
+if (isset($_POST['multiply'])) {
+$operator = $_POST['multiply'];
+}
+if (isset($_POST['divide'])) {
+$operator = $_POST['divide'];
+}
+}
+
 if($_POST['operator']!="") {
 if (isset($_POST['n'])) {
 $operand_2 = $_POST['n'];
@@ -49,33 +70,20 @@ $operand_2 = $_POST['n'];
 }
 
 if($_POST['operator']!="" AND $_POST['operand_2']!="") {
-$answer = $_POST['operand_1'] + $_POST['operand_2'];
+	if($operator=="+") {
+		$answer = $_POST['operand_1'] + $_POST['operand_2'];
+	}
+	if($operator=="-") {
+		$answer = $_POST['operand_1'] - $_POST['operand_2'];
+	}
+	if($operator=="*") {
+		$answer = $_POST['operand_1'] * $_POST['operand_2'];
+	}
+	if($operator=="/") {
+		$answer = $_POST['operand_1'] / $_POST['operand_2'];
+	}
 }
 
-$operator = $_POST['operator'];
+showCalculatorForm($operand_1, $operator, $operand_2, $answer);
 
-$calulator_form = "
-<form method='post'>
-<input type='text' name='operand_1' value=$operand_1>
-<input type='text' name='operator' value=$operator>
-<input type='text' name='operand_2' value=$operand_2></br>
-<input type='text' name='answer' value=$answer></br>
-<input type='submit' name='n' value=1>
-<input type='submit' name='n' value=2>
-<input type='submit' name='n' value=3></br>
-<input type='submit' name='n' value=4>
-<input type='submit' name='n' value=5>
-<input type='submit' name='n' value=6></br>
-<input type='submit' name='n' value=7>
-<input type='submit' name='n' value=8>
-<input type='submit' name='n' value=9></br>
-<input type='submit' name='n' value=0></br>
-<input type='submit' name='operator' value='+'></br>
-<input type='submit' name='operator' value='-'></br>
-<input type='submit' name='operator' value='*'></br>
-<input type='submit' name='operator' value='/'>
-<input type='submit' name='equals' value='='>
-</form>";
 }
-
-echo $calulator_form;
